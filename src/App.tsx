7bcb9c8644625a2e26be49ab25fca4f4e4d32cc4
mnell <mtothenell@gmail.com>
2024-05-27 11:35:24 +0200

    ....

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,28 @@ import {
     Box,
     Text,
     theme,
-    Image
+    Image, Divider, Link
 } from "@chakra-ui/react"
 import mangoDesign from './md1.png';
 
 export const App = () => (
     <ChakraProvider theme={theme}>
 
-        <Box display="flex"
-             flexDirection="column"
-             justifyContent="center"
-             alignItems="center"
-             height="100vh"
-        >
-            <Image src={mangoDesign} alt="Mango design" mb={0}></Image>
-            <Text>
-                "Creating stylish, functional, and custom made applications"
-            </Text>
-            <Text>
-                Contact Ola Magnell at mtothenell@gmail.com
-            </Text>
-        </Box>
-
+            <Box display="flex"
+                 flexDirection="column"
+                 justifyContent="center"
+                 alignItems="center"
+                 height="100vh"
+                 width="100%"
+                 textAlign="center"
+            >
+                <Divider orientation="horizontal" height="10px" width="100vh" bg="black"/>
+                <Image src={mangoDesign} alt="Mango design" mb={0}></Image>
+                <Link href="mailto:mtothenell@gmail.com">
+                    Making stylish, functional, and secure applications.
+                </Link>
+                <Divider orientation="horizontal" height="10px" width="100vh" bg="black"/>
+            </Box>
     </ChakraProvider>
 );
 
